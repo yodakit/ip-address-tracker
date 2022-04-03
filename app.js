ce3680d5ebb4188964e1c14af8a5bb7aad4cada7
Alex <nohacvvi@gmail.com>
2022-04-03 17:51:46 +0300

    connecting Yandex maps

---
+++ app.js
@@ -0,0 +1,14 @@
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [59.93772, 30.313622],
+    zoom: 16,
+  });
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+}
+ymaps.ready(init);
