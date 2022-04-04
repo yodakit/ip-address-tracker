6fad7acf72c00762c7ae3069c702d79245509233
Alex <nohacvvi@gmail.com>
2022-04-04 17:18:23 +0300

    Drawing data, getting user data via ip

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Variables
 const formInput = document.getElementById('form'),
-      input = document.getElementById('input');
+      input = document.getElementById('input'),
+      locationElem = document.getElementById('location'),
+      timezoneElem = document.getElementById('timezone');
+
 
 let lat, lng, city, region, postalCode, timezone;
 
-const geo = navigator.geolocation;
-
-// Browser user geo
-geo.getCurrentPosition(geoUser);
+// Data user
+getDataUser();
 
 // IP receive event
 formInput.addEventListener('submit', (event) => {
@@ -19,34 +20,58 @@ formInput.addEventListener('submit', (event) => {
 
 // Function
 
-// Get user geo
-function geoUser(GeolocationPosition) {
-  const {coords: {latitude, longitude}} = GeolocationPosition;
-  [lat, lng] = [latitude, longitude];
-  ymaps.ready(init);
+// Gettting data user
+function getDataUser() {
+  fetch('https://ipapi.co/json/')
+  .then(res => res.json())
+  .then(data => destrData(data, true));
 }
 
 // Getting data from ip
-function getDataFromIp(ipAdrress) {
+
+function getDataFromIp(ipAddress) {
   fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_PxkTXrm276OiicMW67ucF7GmSPjci&ipAddress=${ipAddress}`)
     .then(res => res.json())
-    .then(data => destrData(data));
+    .then(data => destrData(data, false));
 }
 
 // Data destructuring
-function destrData(data) {
-  console.log(data);
-  ({location: {lat, lng}} = data);
+function destrData(data, isUserData) {
+  if (isUserData) {
+    ({city, region} = data);
+    [lat, lng, postalCode] = [data.latitude, data.longitude, data.postal];
+    timezone = data.utc_offset.slice(0, 3) + ':00';
+  } else {
+    ({location: {lat, lng, city, region, postalCode, timezone}} = data);
+  }
+
+  renderData();
   ymaps.ready(init);
+}
+
+// Render data
+function renderData() {
+  const newLocation = `${city}, ${region} ${postalCode}`;
+  
+  locationElem.innerText = newLocation;
+  timezoneElem.innerText = timezone;
 }
 
 // Creating map
 function init() {
   let map = new ymaps.Map('map', {
     center: [lat, lng],
-    zoom: 16,
+    zoom: 10,
   });
 
+  map.geoObjects.add(new ymaps.Placemark([lat, lng], {
+    balloonContent: 'То самое место'
+  }, {
+    preset: 'islands#circleIcon',
+    iconColor: '#3caa3c'
+  }));
+
+  // Remove previous map
   const prevMap = document.getElementById('map').firstElementChild;
   const newMap = document.getElementById('map').lastElementChild;
 
@@ -59,7 +84,6 @@ function init() {
   map.controls.remove('trafficControl'); // удаляем контроль трафика
   map.controls.remove('typeSelector'); // удаляем тип
   map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
   map.controls.remove('rulerControl'); // удаляем контрол правил
 }
 
